46f6d6a104e6e0602597b20ec1fe56b641ee0e9d
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-03 14:54:40 +0200

    Refactor user input handling and enhance game logic for pari/dispari

--- js-pari-dispari/script.js
+++ js-pari-dispari/script.js
@@ -1,9 +1,12 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). Dichiariamo chi ha vinto.
 
 // Facciamo scegliere all'utente pari o dispari e poi un numero da 1 a 5. Creiamo due prompt.
-// const userChoice = prompt("Pari o dispari?");
+let userChoice = prompt("Pari o dispari?");
+console.log(`L'utente ha scelto: ${userChoice}`);
 const userNumber = Number(prompt("Scegli un numero da 1 a 5."));
 console.log(`L'utente ha scelto il numero: ${userNumber}`);
+
+if (1 >= userNumber <= 5) { }
 
 // Generiamo un numero random per il computer usando una funzione.
 function getRandomNumber(min, max) {
@@ -14,7 +17,7 @@ console.log(`Il numero generato per il computer è: ${computerNumber}`);
 
 // Sommiamo i due numeri.
 const somma = userNumber + computerNumber;
-console.log(`La somma dei due numeri è: ${somma}`);
+console.log(`La somma dei numeri è: ${somma}`);
 
 // Stabiliamo se la somma è pari o dispari tramite una funzione.
 function pariDispari(numb) {
@@ -27,5 +30,20 @@ function pariDispari(numb) {
     }
     return pariDispari;
 }
-
 console.log(pariDispari(somma));
+
+//Dichiariamo chi ha vinto!
+if ((userChoice = "Pari" || "pari") && (somma % 2 === 0)) {
+    // alert("Complimenti! Hai vinto!");
+    console.log("Ha vinto l'utente.");
+
+}
+else if ((userChoice = "Dispari" || "dispari") && (somma % 2 != 0)) {
+    // alert("Complimenti! Hai vinto!");
+    console.log("Ha vinto l'utente.");
+}
+else {
+    // alert("Peccato! Questa volta ho vinto io!");
+    console.log("Ha vinto il computer.");
+}
+
