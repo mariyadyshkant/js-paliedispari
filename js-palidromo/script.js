4f8c8f3d1c5fd107d7ef73767100589f129d587e
mariyadyshkant <mariya.dyshkant@gmail.com>
2025-06-03 17:11:58 +0200

    Refactor user choice handling and implement functions to determine parity for game logic

--- js-palidromo/script.js
+++ js-palidromo/script.js
@@ -1,1 +1,19 @@
 // Chiedere all’utente di inserire una parola. Creare una funzione per capire se la parola inserita è palindroma.
+
+// Chiedo all'utente di inserire una parola qualsiasi.
+// const userWord = prompt("Scrivi una parola");
+const reverseWord = "";
+
+function palindromeWord(word) {
+    for (let i = 0; i < word.legth; i++) {
+        let thisLetter = word[i];
+        console.log(`${thisLetter}, i`);
+    }
+    for (let i = word.legth - 1; i > 0; i--) {
+        let this lett
+    }
+    return palindromeWord;
+}
+
+console.log(palindromeWord("lattina"));
+
--- js-pari-dispari/script.js
+++ js-pari-dispari/script.js
@@ -1,17 +1,16 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri. Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione). Dichiariamo chi ha vinto.
 
 // Facciamo scegliere all'utente pari o dispari e poi un numero da 1 a 5. Creiamo due prompt.
-let userChoice = prompt("Pari o dispari?");
-console.log(`L'utente ha scelto: ${userChoice}`);
+const userChoice = prompt("Pari o dispari?");
 const userNumber = Number(prompt("Scegli un numero da 1 a 5."));
 console.log(`L'utente ha scelto il numero: ${userNumber}`);
 
-if (1 >= userNumber <= 5) {
-    console.log("Continuiamo...");
+// if (1 >= userNumber <= 5) {
+//     console.log("Continuiamo...");
 
-} else {
-    alert("Attenzione! Il numero da te scelto deve essere compreso tra 1 e 5.")
-}
+// } else {
+//     alert("Attenzione! Il numero da te scelto deve essere compreso tra 1 e 5.")
+// }
 
 // Generiamo un numero random per il computer usando una funzione.
 function getRandomNumber(min, max) {
@@ -24,31 +23,40 @@ console.log(`Il numero generato per il computer è: ${computerNumber}`);
 const somma = userNumber + computerNumber;
 console.log(`La somma dei numeri è: ${somma}`);
 
-// Stabiliamo se la somma è pari o dispari tramite una funzione.
-function pariDispari(numb) {
-    if (numb % 2 === 0) {
-        console.log("La somma dei numeri è: pari");
-
-    } else {
-        console.log("La somma dei numeri è: dispari");
-
+// Creiamo una funzione per stabilire se la scelta dell'utente sia stata pari o dispari.
+function userPariDispari(choice) {
+    let = pari_dispari = "";
+    if (choice === "Pari" || choice === "pari") {
+        pari_dispari = "pari";
+    } else if (choice === "Dispari" || choice === "dispari") {
+        pari_dispari = "dispari"
     }
-    return pariDispari;
+    return pari_dispari;
 }
-console.log(pariDispari(somma));
+console.log(`L'utente ha scelto: ${userPariDispari(userChoice)}`);
+
+// Stabiliamo se la somma è pari o dispari tramite un'altra funzione.
+function sommaPariDispari(numb) {
+    let pari_dispari = numb % 2 === 0;
+    if (pari_dispari = true) {
+        pari_dispari = "pari";
+    } else {
+        pari_dispari = "dispari";
+    }
+    return pari_dispari;
+}
+console.log(`La somma è: ${sommaPariDispari(somma)}`); // fnc
+
 
 //Dichiariamo chi ha vinto!
-if ((userChoice = "Pari" || "pari") && (somma % 2 === 0)) {
-    // alert("Complimenti! Hai vinto!");
+if ((userPariDispari(userChoice) == "pari") && (sommaPariDispari(somma) == "pari")) {
     console.log("Ha vinto l'utente.");
-
-}
-else if ((userChoice = "Dispari" || "dispari") && (somma % 2 != 0)) {
-    // alert("Complimenti! Hai vinto!");
+    alert("Complimenti! Hai vinto!");
+} else if ((userPariDispari(userChoice) == "dispari") && (sommaPariDispari(somma) == "dispari")) {
     console.log("Ha vinto l'utente.");
-}
-else {
-    // alert("Peccato! Questa volta ho vinto io!");
+    alert("Complimenti! Hai vinto!");
+} else {
     console.log("Ha vinto il computer.");
+    alert("Peccato! Questa volta ho vinto io!");
 }
 
